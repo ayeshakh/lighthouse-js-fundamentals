9a9cd8b17d9e7172b96399ffc68582796e6564ca
Ayesha <ayesha_04@yahoo.com>
2017-05-06 13:36:05 +0000

    Adds conditionals excercise

---
+++ go-outside.js
@@ -0,0 +1,16 @@
+var temperature = 12;
+
+if (temperature < 0)
+ {
+  console.log("Make sure you pick out a scarf!");
+}
+ else if (temperature < 15)
+ {
+  console.log("Short sleeves wont cut it.");
+ }
+ else
+ {
+  console.log("short sleeves are fine");
+ }
+
+ console.log("Now you are ready to go outside");
