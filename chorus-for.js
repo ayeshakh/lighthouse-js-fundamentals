57b0642f0a079dbe2d4ac8641614633b5bcfe151
Ayesha <ayesha_04@yahoo.com>
2017-05-07 14:42:49 +0000

    Adds looping excercises

---
+++ chorus-for.js
@@ -0,0 +1,6 @@
+var chorus = "Let's Dance";
+for (var repeat =0; repeat < 10; repeat ++)
+{
+  console.log(chorus);
+}
+console.log("until the sun come up");
---
+++ chorus-while.js
@@ -0,0 +1,12 @@
+var chorus = "Lets Dance!";
+var repeat = 0;
+while(repeat < 10)
+{
+  if (repeat === 5)
+  {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
